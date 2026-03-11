79f4c45787b7c1363ef1a8c7dab62029bbe94827
jerryou8 <526089989@qq.com>
2026-03-11 21:27:25 +0800

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,1 +1,11 @@
 
+Config = {}
+
+function Config:GetDefault()
+    return RaidBetMasterDB.config or {
+        defaultOdds = 10,
+        defaultBet = 100,
+        defaultTimer = 30,
+        maxHistory = 10
+    }
+end
